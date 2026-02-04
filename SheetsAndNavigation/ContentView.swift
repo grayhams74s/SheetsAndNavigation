82fece4a809cdd2d6c38fb95a926baf82834666d
grayhams74s <felixjunang@gmail.com>
2026-02-04 14:20:39 +0800

    Initial Commit

---
+++ SheetsAndNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheetsAndNavigation
+//
+//  Created by felix angcot jr on 2/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SheetsAndNavigation/SheetsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetsAndNavigationApp.swift
+//  SheetsAndNavigation
+//
+//  Created by felix angcot jr on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SheetsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
